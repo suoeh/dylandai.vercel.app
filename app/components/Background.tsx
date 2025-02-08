40fa3a999927b908c591a30f1f5bda24da69d528
suoeh <dylanydai@gmail.com>
2025-02-07 19:40:15 -0500

    ALMOST DONE WEBSITE!!!
    added cool background

--- app/components/Background.tsx
+++ app/components/Background.tsx
@@ -1,9 +1,10 @@
+import Image from "../media/background.png"
 export default function Background() {
   return (
     <div 
       className="w-screen h-screen bg-center bg-cover bg-no-repeat -z-10"
       style={{
-        backgroundImage: `url('https://media.newyorker.com/photos/59095c67ebe912338a37455d/master/pass/Stokes-Hello-Kitty2.jpg')`,
+        backgroundImage: `url(${Image.src})`,
         height: '300%'
       }}
     />
